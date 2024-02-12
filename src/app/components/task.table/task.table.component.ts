9f1c6a2764246b75091315538b0eb3a62ab92612
Jacked <jaceb134@gmail.com>
2024-02-12 13:12:49 +0100

    card with taks

--- src/app/components/task.table/task.table.component.ts
+++ src/app/components/task.table/task.table.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
-  selector: 'app-task.table',
+  selector: 'app-task-table',
   standalone: true,
-  imports: [MatListModule],
+  imports: [MatListModule, MatCardModule],
   templateUrl: './task.table.component.html',
   styleUrl: './task.table.component.scss',
 })
