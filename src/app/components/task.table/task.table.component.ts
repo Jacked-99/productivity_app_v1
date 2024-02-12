c0d4810b8cbc6ed2f41a3cbffb6fdcd2f00f6d75
Jacked <jaceb134@gmail.com>
2024-02-12 12:48:39 +0100

    testing dispaly

--- src/app/components/task.table/task.table.component.ts
+++ src/app/components/task.table/task.table.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-task.table',
   standalone: true,
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './task.table.component.html',
-  styleUrl: './task.table.component.scss'
+  styleUrl: './task.table.component.scss',
 })
-export class TaskTableComponent {
-
-}
+export class TaskTableComponent {}
