b5d374aa1b593e2fa89d154eb4a66ee889b3ff6f
Jacked <jaceb134@gmail.com>
2024-02-12 14:10:43 +0100

    added styles for create task button

--- src/app/components/taks-table-button/taks-table-button.component.ts
+++ src/app/components/taks-table-button/taks-table-button.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-taks-table-button',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './taks-table-button.component.html',
-  styleUrl: './taks-table-button.component.scss'
+  styleUrl: './taks-table-button.component.scss',
 })
-export class TaksTableButtonComponent {
-
-}
+export class TaksTableButtonComponent {}
--- src/app/components/taks-table-column/taks-table-column.component.ts
+++ src/app/components/taks-table-column/taks-table-column.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCard, MatCardModule } from '@angular/material/card';
 import { TaskTableComponent } from '../task.table/task.table.component';
+import { TaksTableButtonComponent } from '../taks-table-button/taks-table-button.component';
 
 @Component({
   selector: 'app-taks-table-column',
   standalone: true,
-  imports: [MatCardModule, TaskTableComponent],
+  imports: [MatCardModule, TaskTableComponent, TaksTableButtonComponent],
   templateUrl: './taks-table-column.component.html',
   styleUrl: './taks-table-column.component.scss',
 })
