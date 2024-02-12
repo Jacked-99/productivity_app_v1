440f4cbe7a0c52bb49a60d39667f43a7f5c724cc
Jacked <jaceb134@gmail.com>
2024-02-12 13:52:32 +0100

    created add task button

---
+++ src/app/components/taks-table-button/taks-table-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taks-table-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './taks-table-button.component.html',
+  styleUrl: './taks-table-button.component.scss'
+})
+export class TaksTableButtonComponent {
+
+}
