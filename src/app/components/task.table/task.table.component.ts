d1bef60597fbfb2ab7e38e315e25f6d7dd9b238b
Jacked <jaceb134@gmail.com>
2024-02-12 14:25:32 +0100

    biolerplate for expanding task panels

--- src/app/components/task.table/task.table.component.ts
+++ src/app/components/task.table/task.table.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 @Component({
   selector: 'app-task-table',
   standalone: true,
-  imports: [MatListModule, MatCardModule],
+  imports: [MatListModule, MatCardModule, MatExpansionModule],
   templateUrl: './task.table.component.html',
   styleUrl: './task.table.component.scss',
 })
