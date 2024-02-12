fb418be4f8a5debfef627a8b0ecf5111c3b49db9
Jacked <jaceb134@gmail.com>
2024-02-12 14:59:11 +0100

    added checkbox

--- src/app/components/task.table/task.table.component.ts
+++ src/app/components/task.table/task.table.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @Component({
   selector: 'app-task-table',
   standalone: true,
-  imports: [MatListModule, MatCardModule, MatExpansionModule],
+  imports: [
+    MatListModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+  ],
   templateUrl: './task.table.component.html',
   styleUrl: './task.table.component.scss',
 })
