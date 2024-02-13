3ec68e456ddb870848c0910c695f8cbce7410882
Jacked <jaceb134@gmail.com>
2024-02-13 11:56:36 +0100

    added dialog coimponent

--- src/app/components/taks-table-column/taks-table-column.component.ts
+++ src/app/components/taks-table-column/taks-table-column.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { MatCard, MatCardModule } from '@angular/material/card';
 import { TaskTableComponent } from '../task.table/task.table.component';
 import { TaksTableButtonComponent } from '../taks-table-button/taks-table-button.component';
+import { TaskDialogComponent } from '../task-dialog/task-dialog.component';
 
 @Component({
   selector: 'app-taks-table-column',
   standalone: true,
-  imports: [MatCardModule, TaskTableComponent, TaksTableButtonComponent],
+  imports: [
+    MatCardModule,
+    TaskTableComponent,
+    TaksTableButtonComponent,
+    TaskDialogComponent,
+  ],
   templateUrl: './taks-table-column.component.html',
   styleUrl: './taks-table-column.component.scss',
 })
---
+++ src/app/components/task-dialog/task-dialog.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Inject } from '@angular/core';
+import { TaskMain } from '../../shared/task-main';
+@Component({
+  selector: 'app-task-dialog',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+  templateUrl: './task-dialog.component.html',
+  styleUrl: './task-dialog.component.scss',
+})
+export class TaskDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<TaskDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: TaskMain
+  ) {}
+  onNoclick() {
+    this.dialogRef.close();
+  }
+}
