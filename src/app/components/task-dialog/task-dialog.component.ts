e05933fce0d768f5d0471ace663976927ee4464a
Jacked <jaceb134@gmail.com>
2024-02-15 09:38:41 +0100

    added abiality to add and display subtasks in task dialog

--- src/app/components/task-dialog/task-dialog.component.ts
+++ src/app/components/task-dialog/task-dialog.component.ts
@@ -16,6 +16,11 @@ import { Inject } from '@angular/core';
 import { TaskMain } from '../../shared/task-main';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Task } from '../../shared/task';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatListModule } from '@angular/material/list';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-task-dialog',
@@ -30,17 +35,41 @@ import { provideNativeDateAdapter } from '@angular/material/core';
     MatDialogActions,
     MatDialogClose,
     MatDatepickerModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './task-dialog.component.html',
   styleUrl: './task-dialog.component.scss',
 })
 export class TaskDialogComponent {
+  taskTitle = '';
+  subTaskCreate = false;
   constructor(
     public dialogRef: MatDialogRef<TaskDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: TaskMain
   ) {}
   onNoclick() {
     this.dialogRef.close();
+  }
+  addSubtaskClick() {
+    let subTaskData: Task = {
+      uid: '',
+      title: '',
+      completed: false,
+      date: this.data.date,
+    };
+    if (!this.subTaskCreate) {
+      this.subTaskCreate = true;
+    }
+    if (!this.data.subTasks) {
+      this.data.subTasks = [];
+    }
+    subTaskData.title = this.taskTitle;
+    subTaskData.uid = uuid();
+
+    this.data.subTasks?.push(subTaskData);
+    console.log(this.data.subTasks);
+    this.taskTitle = '';
   }
 }
