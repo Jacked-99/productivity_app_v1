1ea420e4616d896aaac75f7d6a8677a634b2d462
Jacked <jaceb134@gmail.com>
2024-02-21 08:54:53 +0100

    changed menu button style

--- src/app/components/task-table-menu/task-table-menu.component.ts
+++ src/app/components/task-table-menu/task-table-menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-task-table-menu',
   standalone: true,
-  imports: [MatMenuModule, MatButtonModule],
+  imports: [MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './task-table-menu.component.html',
   styleUrl: './task-table-menu.component.scss',
 })
